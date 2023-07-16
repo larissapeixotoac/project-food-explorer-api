3bee58acdc359bce4311575b319560c8951549a0
Larissa Peixoto <115260502+larissapeixotoac@users.noreply.github.com>
2023-07-16 19:21:40 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,14 @@ import express from 'express'
 const cors = require('cors')
 
 import routes from './routes'
-const database = require('./database/sqlite')
+// const database = require('./database/sqlite')
 const uploadConfig = require('./configs/upload')
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3333
 
-database()
+// database()
 
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(cors())
