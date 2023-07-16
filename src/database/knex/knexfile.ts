1f09874bdfe18fbdbd5b3deed6f8410a4f72447c
Larissa Peixoto <115260502+larissapeixotoac@users.noreply.github.com>
2023-07-16 19:22:24 -0300

    Add files via upload

--- src/database/knex/knexfile.ts
+++ src/database/knex/knexfile.ts
@@ -24,7 +24,7 @@ export const development: Knex.Config = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-        filename: path.resolve(__dirname, '..', '..', '..', 'food-explorer.sqlite'),
+        filename: path.resolve(__dirname, '..', 'food-explorer.sqlite'),
     },
     migrations: {
         directory: path.resolve(__dirname, 'migrations')
